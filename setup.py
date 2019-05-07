17a14482d12e3570ae7f3e0144e1abf91a861d97
xncpedro <50370246+xncpedro@users.noreply.github.com>
2019-05-07 15:00:12 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='Your Name',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      py_modules=['hello'],
+     )
